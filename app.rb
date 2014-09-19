97b754d1c2cfaa0b1961d55cb09faabcd0906a3d
Victor Rodrigues <victorc.rodrigues@gmail.com>
2014-09-19 10:39:17 +0200

    Base implementation

--- app.rb
+++ app.rb
@@ -2,6 +2,18 @@ require 'sinatra'
 require 'en'
 require 'jenkins_api_client'
 require 'ostruct'
+require 'pry'
+
+get '/webhook' do
+  'in primis cogitationibus circa generationem animalium, ' +
+  'de his omnibus non cogitavi.'
+end
+
+post '/webhook' do
+  pr = PullRequest.new(params)
+  Jenkins.build(pr) if pr.mergeable?
+  status 200
+end
 
 Catatau = EN::Namespace.new(:catatau)
 
@@ -12,18 +24,58 @@ Config = OpenStuct.new(
   templates:        Catatau.fetch(:templates).split(',')
 )
 
-def jenkins_client
-  JenkinsApi::Client.new(
-    server_url: Config.jenkins_url,
-    username:   Config.jenkins_username,
-    password:   Config.jenkins_password
-  )
+class PullRequest
+  attr_reader :action, :sha, :number
+
+  def initialize(params)
+    @action     = params[:action]
+    @merged     = params[:pull_request][:merged]
+    @mergeable  = params[:pull_request][:mergeable]
+    @sha        = params[:pull_request][:head][:sha]
+    @number     = params[:pull_request][:number]
+    @branch_url = params[:repo][:branches_url]
+  end
+
+  def merged?
+    @merged
+  end
+
+  def mergeable?
+    @mergeable
+  end
+
+  def branch
+    @branch ||= @branch_url.gsub(/\A(.*)\/branches\//, '')
+  end
 end
 
-get '/webhook' do
-  'in primis cogitationibus circa generationem animalium, ' +
-  'de his omnibus non cogitavi.'
-end
+module Jenkins
+  extend self
 
-post '/webhook' do
+  def build(pr)
+    jenkins = client
+
+    Config.templates.each do |template|
+      name   = job_name(template, pr)
+      config = jenkins.job.get_config(template)
+      jenkins.job.create_or_update(name, config)
+      jenkins.job.build(name, sha: pr.sha)
+    end
+  end
+
+  def templates
+    Config.templates
+  end
+
+  def job_name(template, pr)
+    "pr_#{pr.number}_template_#{pr.branch}"
+  end
+
+  def client
+    JenkinsApi::Client.new(
+      server_url: Config.jenkins_url,
+      username:   Config.jenkins_username,
+      password:   Config.jenkins_password
+    )
+  end
 end
